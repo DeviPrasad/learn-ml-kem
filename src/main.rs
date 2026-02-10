497b7ee609403021d76f2cd47d2f6a9752b24df0
Devi Prasad <dp@web3pleb.org>
2026-02-10 15:38:56 +0530

    Encoders and Decoders of different bit-wdths implemented. Refactored codecs into a separate module.
    Added a couple references.
    
    One of the works by Bo Lin is of interest to us. We should
    communicate our own diagrams and annotated references there.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use crate::field::FieldElement;
 use crate::params::{DU, Q};
 
+mod codec;
 mod field;
 mod params;
 mod ring;
@@ -9,6 +10,6 @@ fn main() {
     assert!(DU < 12);
     for x in 0..Q {
         let t = FieldElement::from(x).compress::<DU>();
-        assert!(u16::from(t) <= (1 << DU)-1)
+        assert!(u16::from(t) <= (1 << DU) - 1)
     }
 }
